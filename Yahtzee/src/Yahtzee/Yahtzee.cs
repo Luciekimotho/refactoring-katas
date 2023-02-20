a35b867e2418cfcd8bf2ba79f71941fda561fbf8
Lucy Kimotho <kimotholucy@microsoft.com>
2023-02-20 21:39:54 +0300

    consistent function naming, add another yahzee round function

--- Yahtzee/src/Yahtzee/Yahtzee.cs
+++ Yahtzee/src/Yahtzee/Yahtzee.cs
@@ -14,23 +14,26 @@ public class Yahtzee
     public static int Chance(int d1, int d2, int d3, int d4, int d5)
     {
         int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+        return total + d1 + d2 + d3 + d4 + d5;
     }
 
-    public static int yahtzee(params int[] dice)
+    public static int YahtzeeRound2(params int[] dice)
     {
         int[] counts = new int[6];
         foreach (int die in dice)
             counts[die - 1]++;
-        for (int i = 0; i != 6; i++)
-            if (counts[i] == 5)
+        for (int i = 0; i <= dice.Length; i++)
+            if (counts[i] == dice.Length)
                 return 50;
         return 0;
+    }
+
+    public static int YahtzeeRound(params int[] dice)
+    {
+        foreach (int die in dice)
+            if (die != dice[0])
+                return 0;
+        return 50;
     }
 
     public static int Ones(int d1, int d2, int d3, int d4, int d5)
